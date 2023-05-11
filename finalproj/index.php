50f31824776e4276ca3cff3c74b3ce9b4b57172c
ayayaka <94544383+ayayaka@users.noreply.github.com>
2023-05-11 23:10:52 +0800

    Update index.php

--- finalproj/index.php
+++ finalproj/index.php
@@ -172,7 +172,7 @@
 
 <footer class="text-center text-white" style="background-color: #f1f1f1;">
   <!-- Grid container -->
-  <div class="container pt-4" style="width=100%">
+  <div class="container-fluid pt-4" style="width=100%">
     <!-- Section: Social media -->
     <section class="mb-4">
     <!-- FACEBOOK -->
@@ -181,6 +181,7 @@
         href="https://www.facebook.com/paolo.porte.50"
         role="button"
         data-mdb-ripple-color="dark"
+        style="font-size: 30px"
         ><span class="bi bi-facebook"></span></a>
 
 
@@ -190,6 +191,7 @@
         href="https://www.instagram.com/_trippyvault/"
         role="button"
         data-mdb-ripple-color="dark"
+        style="font-size: 30px"
         ><span class="bi bi-instagram"></span></a>
 
         <!-- PHONE -->
@@ -197,8 +199,9 @@
         class="btn btn-link btn-floating btn-lg text-info m-1"
         href="#!"
         role="button"
-        data-mdb-ripple-color="dark">
-        <span class="bi bi-telephone"></span></a>
+        data-mdb-ripple-color="dark"
+        style="font-size: 30px"
+        ><span class="bi bi-telephone"></span></a>
         
     </section>
     <!-- Section: Social media -->
@@ -207,9 +210,9 @@
 
   <!-- Copyright -->
   
-  <div class="text-right text-info p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-   <span class="text-right  p-3">© 2023 Copyright: Trippy Vault</span>
-   <span class="float-left  p-1"><span class="bi bi-geo-alt"></span> Pagbilao, Quezon</span>
+  <div class="container-fluid text-right text-info p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+  <span class="text-right mx-2" style="font-size: 18px"><span class="bi bi-geo-alt"></span> Pagbilao, Quezon</span>
+  <span class="text-right mx-2"  style="font-size: 18px">© 2023 Copyright: Trippy Vault</span> 
   </div>
   <!-- Copyright -->
 </footer>
